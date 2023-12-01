39058a325737c4080be449102624b7aeaf00a455
Danis Art <nikita.efremov1412@gmail.com>
2023-12-01 15:32:24 +0300

    ДЗ 2023.11.23

---
+++ 2023.11.23/1.py
@@ -0,0 +1,14 @@
+num_1 = float(input())
+num_2 = float(input())
+num_3 = float(input())
+sum_nat_num = 0
+for n in (num_1, num_2, num_3):
+    if n > 0:
+        sum_nat_num += n
+print(sum_nat_num)
+
+# 23
+# -16
+# 4.8
+# 27.8
+
