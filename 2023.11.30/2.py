133ae8c7d84ec9857550ec886e4f5a7c55950848
Danis Art <nikita.efremov1412@gmail.com>
2023-12-18 23:20:21 +0300

    Выполненное дз

---
+++ 2023.11.30/2.py
@@ -0,0 +1,17 @@
+n = int(input())
+sum_numbers = 0
+for i in range(n):
+    num = int(input())
+    if num > 0:  
+        sum_numbers += num
+print(sum_numbers)
+
+# 7
+# -23
+# 0
+# 12
+# 34
+# -5421
+# 2
+# -5
+# 48
