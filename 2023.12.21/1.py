ba5434e262054f0546c2754a880ff66de5ab537b
Danis Art <nikita.efremov1412@gmail.com>
2024-01-05 02:12:18 +0300

    Выполненное дз

--- 2023.12.21/1.py
+++ 2023.12.21/1.py
@@ -15,34 +15,31 @@ nominals = {
         100, 102, 105, 107, 110, 113, 115, 118, 121, 124, 127, 130, 133, 137, 140, 143, 147, 150, 154, 158, 162, 165, 169, 174, 178, 182, 187, 191, 196, 200, 205, 210, 215, 221, 226, 232, 237, 243, 249, 255, 261, 267, 274, 280, 287, 294, 301, 309, 316, 324, 332, 340, 348, 357, 365, 374, 383, 392, 402, 412, 422, 432, 442, 453, 464, 475, 487, 499, 511, 523, 536, 549, 562, 576, 590, 604, 619, 634, 649, 665, 681, 698, 715, 732, 750, 768, 787, 806, 825, 845, 866, 887, 909, 931, 953, 976
     )
 }
-
+percent = {'E6': 0.2, 'E12': 0.1, 'E24': 0.05, 'E48': 0.02, 'E96': 0.01}
 def pick_resistors(num: int) -> dict | None:
-    E6 = 0.2
-    E12 = 0.1
-    E24 = 0.05
-    E48 = 0.02
-    E96 = 0.01
-    
-    a = [E6, E12, E24, E48, E96]
-    b = [num*i for i in a] # Процент расхождения от заявленного номинала
-    c = [(num-i, num+i) for i in b] # пара чисел с отклонением 
-    d = [i for i in nominals]
-    print(d)
-    e = [(c[i], d[i]) for i in range(5)]
-    print(e)
-    for i, j in e:  
-        print(i, j)
-        print(find_min_nom(i, nominals[j]))
+    c = {}
+    for nominal in nominals:
+        c[nominal] = calc(num, percent[nominal], nominals[nominal])
+    if 100 <= num <= 999:
+        return c
+    else:
+        return None
+        
+def calc(num: int, nom_percent: float, nominal: str) -> tuple:
+    """ Находит номинал с наименьшим отклонением от заданных отклонений """
+    num1 = num-num*nom_percent
+    num2 = num+num*nom_percent
+    min_diff1 = min([abs(num1-i) for i in nominal])
+    min_diff2 = min([abs(num2-i) for i in nominal])
 
-def find_min_nom(num: tuple, nominal: tuple):
+    if 100 < num1 > 999:
+        min_diff = min_diff2
+    elif 100 < num2 > 999:
+        min_diff = min_diff1
+    else:
+        min_diff = min([min_diff1, min_diff1])
         
-        min_diff_1_value = min([abs(num[0]-n) for n in nominal]) # минимальная разница между числом 1 и номиналом
-        min_diff_2_value = min([abs(num[1]-n) for n in nominal]) # минимальная разница между числом 2 и номиналом
-        min_diff = min(min_diff_1_value, min_diff_2_value)
-        
-        nominal_value = [n for n in nominal if abs(num[0]-n) == min_diff or abs(num[1]-n) == min_diff]
-        
-        return nominal_value
-    # print(b)
-    # print(c)
-    # return c
+    c1 = filter(lambda x: abs(x-num1)==b3, nominal)
+    c2 = filter(lambda x: abs(x-num2)==b3, nominal)
+    return tuple(c1)+tuple(c2)
+    
