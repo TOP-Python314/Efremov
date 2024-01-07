1daa0d0207ffeb9c34828f8381f49bd847b3367c
Danis Art <nikita.efremov1412@gmail.com>
2024-01-07 12:21:33 +0300

    Выполненное ДЗ

--- 2023.11.23/6.py
+++ 2023.11.23/6.py
@@ -1,16 +1,16 @@
-coord_1 = input()
-coord_2 = input()
+coord1 = input()
+coord2 = input()
 
-coord_1_int = int(coord_1[1])
-coord_1_ltr = coord_1[0]
-coord_2_int = int(coord_2[1])
-coord_2_ltr = coord_2[0]
+coord1_int = int(coord1[1])
+coord1_ltr = coord1[0]
+coord2_int = int(coord2[1])
+coord2_ltr = coord2[0]
 
-if (coord_1 != coord_2 and
-        1 <= coord_1_int <= 8 and 'a' <= coord_1_ltr <= 'h' and
-        1 <= coord_2_int <= 8 and 'a' <= coord_2_ltr <= 'h' and
-        (-1 <= coord_1_int - coord_2_int <= 1) and 
-        (-1 <= ord(coord_1_ltr) - ord(coord_2_ltr) <= 1)):
+if (coord1 != coord2 and
+        1 <= coord1_int <= 8 and 'a' <= coord1_ltr <= 'h' and
+        1 <= coord2_int <= 8 and 'a' <= coord2_ltr <= 'h' and
+        (-1 <= coord1_int - coord2_int <= 1) and 
+        (-1 <= ord(coord1_ltr) - ord(coord2_ltr) <= 1)):
     print('да')
 else:
     print('нет')
