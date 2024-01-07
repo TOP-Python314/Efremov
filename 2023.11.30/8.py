824d63e6cb58a497f3518f7f1d707cb75e84e453
Danis Art <nikita.efremov1412@gmail.com>
2024-01-07 12:23:16 +0300

    Выполненное ДЗ

--- 2023.11.30/8.py
+++ 2023.11.30/8.py
@@ -1,11 +1,11 @@
 n = int(input())
 
-num_1 = 1
-num_2 = 1
+num1 = 1
+num2 = 1
 
 for i in range(n):
-    print(num_1, end=' ')
-    num_1, num_2 = num_2, num_1 + num_2
+    print(num1, end=' ')
+    num1, num2 = num2, num1 + num2
     
 # 21
 # 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946
