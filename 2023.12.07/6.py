2339621e0480b7b181364a396b67cb91ec5e54b8
Danis Art <nikita.efremov1412@gmail.com>
2024-01-07 12:25:26 +0300

    Выполненное ДЗ

--- 2023.12.07/6.py
+++ 2023.12.07/6.py
@@ -1,12 +1,12 @@
-set_1 = input()
-set_2 = ['1', '0', 'b']
+set1 = input()
+set2 = ['1', '0', 'b']
 
-if set(set_1) - set(set_2) != set():
+if set(set1) - set(set2) != set():
     print('нет')
 else:
-    if (set_1[0] == 'b' or
-            set_1[:2] == '0b' or
-            set(set_1) - {'1', '0'} == set()):
+    if (set1[0] == 'b' or
+            set1[:2] == '0b' or
+            set(set1) - {'1', '0'} == set()):
         print('да')
     else:
         print('нет')
