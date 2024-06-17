f462caf0da835a2dfb2ae4b66f52f12f637d3e65
Efremov Nikita <119881370+DanisArt13@users.noreply.github.com>
2024-06-17 20:59:53 +0300

    Delete first_project directory

--- first_project/draw.py
+++
@@ -1,5 +0,0 @@
-# field_template = ' {} | {} | {} \n———————————\n {} | {} | {} \n———————————\n {} | {} | {} '
-def field_template(cells):
-    line = (cells-1) * ' {} |' + ' {} \n'
-    a = line + (cells-1) * '————' + '———\n'   
-    return a * (cells-1) + line
